3762c6c231622707bfb18b10c359072048746006
Brendon Wilcox <mrw321@gmail.com>
2021-02-01 17:11:18 -0500

    Changed Header Links

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -26,12 +26,12 @@ class Header extends Component {
                                 </NavLink>
                             </NavItem>
                             <NavItem className="vertCenter pt-3">
-                                <NavLink className="nav-link text-dark" to="/aboutme" href="/aboutme/">
+                                <NavLink className="nav-link text-dark" to="/" href="/aboutme/">
                                     <i className="fa fa-home fa-lg" /> About Me
                                 </NavLink>
                             </NavItem>
                             <NavItem className="vertCenter pt-3">
-                                <NavLink className="nav-link text-dark" to="/projects" href="/projects">
+                                <NavLink className="nav-link text-dark" to="/" href="/projects">
                                     <i className="fa fa-home fa-lg" /> Projects
                                 </NavLink>
                             </NavItem>
